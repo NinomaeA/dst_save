963610ec7739ae128684f4b8bed5f423b2a6057b
NinomaeA <arcanecarlos@gmail.com>
2021-01-15 10:30:01 +0800

    renew mod list and save upload

---
+++ dedicated_server_mods_setup.lua
@@ -0,0 +1,24 @@
+--There are two functions that will install mods, ServerModSetup and ServerModCollectionSetup. Put the calls to the functions in this file and they will be executed on boot.
+
+--ServerModSetup takes a string of a specific mod's Workshop id. It will download and install the mod to your mod directory on boot.
+	--The Workshop id can be found at the end of the url to the mod's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
+	--ServerModSetup("350811795")
+
+--ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
+	--The Workshop id can be found at the end of the url to the collection's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
+	--ServerModCollectionSetup("379114180")
+
+
+ServerModSetup("362175979") --WormHoleMarks
+--ServerModSetup("375859599") --HealthInfo
+ServerModSetup("378160973") --GlobalPositions
+ServerModSetup("543945797") --DamageIndicator
+ServerModSetup("797304209") --NoGrassgekko
+ServerModSetup("831523966") --999 stack
+ServerModSetup("1247040097") --XimiRefrigerator
+ServerModSetup("1898292532") --Tips
+ServerModSetup("2283028733") --ExtraEquipSlot
+ServerModSetup("2287303119") --ShowMe
+ServerModSetup("2321974509") --AutomaticChestShorting
